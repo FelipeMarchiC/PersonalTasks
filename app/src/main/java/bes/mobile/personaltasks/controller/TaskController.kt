5fe01a75f7cee1e85ed1f7a8d3a443bad74cf98b
FelipeMarchiC <felipe.marchi3004@gmail.co>
2025-05-18 12:02:38 -0300

    feat: TaskController added

---
+++ app/src/main/java/bes/mobile/personaltasks/controller/TaskController.kt
@@ -0,0 +1,21 @@
+package bes.mobile.personaltasks.controller
+
+import androidx.room.Room
+import bes.mobile.personaltasks.MainActivity
+import bes.mobile.personaltasks.model.Task
+import bes.mobile.personaltasks.model.TaskDao
+import bes.mobile.personaltasks.model.TaskRoomDb
+
+class TaskController(mainActivity: MainActivity) {
+    private val taskDao: TaskDao = Room.databaseBuilder(
+        mainActivity,
+        TaskRoomDb::class.java,
+        name = "task-database"
+    ).build().taskDao()
+
+    fun createTask(task: Task) = taskDao.createTask(task)
+    fun getContact(id: Int) = taskDao.retrieveTask(id)
+    fun getContacts() = taskDao.retrieveTasks()
+    fun updateTask(task: Task) = taskDao.updateTask(task)
+    fun deleteContact(task: Task) = taskDao.deleteTask(task)
+}
